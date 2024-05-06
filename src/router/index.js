6a09c788e4a25149e6f7894a2cdc3ff737d3e376
NikarashiHatsu <yourlovelydev@gmail.com>
2024-05-06 13:29:13 +0700

    chore: remove unused content

--- src/router/index.js
+++ src/router/index.js
@@ -11,17 +11,6 @@ const router = createRouter({
         title: 'Home',
       },
       component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      meta: {
-        title: 'About',
-      },
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     }
   ]
 })
